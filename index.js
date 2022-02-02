c7a015c31b85a536549e55e32a20d353f907d4de
Rochelle Younger <regnuoy321@gmail.com>
2022-02-02 15:25:03 -0500

    added prompt questions and a basic readme template

--- index.js
+++ index.js
@@ -1,13 +1,148 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "confirm",
+        name: "licenseConfirm",
+        message: "Would you like to include a license?",
+        default: false
+    },
+    {
+        type: "rawlist",
+        name: "license",
+        message: "Choose a license to include.",
+        choices: ["MIT", "GNU GPLE v2", "Apache 2.0", "GNU GPL v3", "BSD 3-clause", "ISC", "Perl"],
+        when: ({ licenseConfirm }) => {
+            if (licenseConfirm) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: "list",
+        name: "color",
+        message: "Choose a color for your license badge.",
+        choices: ["lightgrey", "red", "orange", "yellow", "yellowgreen", "green", "brightgreen", "blue", "lightblue", "blueviolet", "violet", "purple", "pink", "hotpink"],
+        when: ({ licenseConfirm }) => {
+            if (licenseConfirm) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project? (Required)",
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log("Field cannot be left empty.");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please describe your project. (Required)",
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log("Field cannot be left empty.");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter the instructions for installation. (Required)",
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log("Field cannot be left empty.");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter information for usage of application. (Required)",
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log("Field cannot be left empty.");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please enter contribution guidelines. (Required)",
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log("Field cannot be left empty.");
+                return false;
+            }
+        }
+    },
+    {
+        type: "confirm",
+        name: "testConfirm",
+        message: "Would you like to include test instructions?",
+        default: false
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please enter test instructions. (Required)",
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log("Field cannot be left empty.");
+                return false;
+            }
+        },
+        when: ({ testConfirm }) => {
+            if (testConfirm) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+];
+
+// prompts user for info
+const userPrompt = () => {
+    return inquirer.prompt(questions);
+};
+// logging data
+const logData = data => console.log(data);
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    userPrompt().then(logData);
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,63 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `![license: ${license}](https://img.shields.io/badge/License-${badge}-${color})
+# ${title}
 
-`;
+## Description
+
+${description}
+
+
+## Table of Contents
+
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Contributing](#contributing)
+  *[Questions](#questions)
+
+
+## Installation
+
+${installation}
+
+
+## Usage
+
+${usage}
+
+
+## License
+
+This application is licensed under ${license}.
+
+
+## Contributing
+
+${contributing}
+
+
+## Questions
+
+  Github: [${github}](https://github.com/${github})
+  Contact Me: [${email}](mailto:${email})
+
+---
+© ${new Date().getFullYear()} ${copyright}`;
 }
 
 module.exports = generateMarkdown;
+
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
